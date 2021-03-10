566a0a6c3587d19514ef76bfe5aca6de444e5dbd
Alex <grin3run@gmail.com>
2021-03-10 11:04:33 +0300

    Functionaly working, need some further styling

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import {weatherModule} from './modules.js'
 
 async function getCurrentData(city) {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e3496c6b36dcdfcc1b4f8ac7e4da9e33`
+    const id = 'e3496c6b36dcdfcc1b4f8ac7e4da9e33'
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${id}`
     const response = await fetch(url);
     if (response.status != 200) {
         return Promise.reject('City not found')
@@ -14,8 +15,9 @@ async function getCurrentData(city) {
 }
 
 async function getMoreData(lon, lat) {
+    const id = 'e3496c6b36dcdfcc1b4f8ac7e4da9e33'
     const unit = weatherModule.returnUnits()
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=${unit}&appid=e3496c6b36dcdfcc1b4f8ac7e4da9e33`
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=${unit}&appid=${id}`
     const response = await fetch(url);
     if (response.status != 200) {
         return Promise.reject('City not found')
